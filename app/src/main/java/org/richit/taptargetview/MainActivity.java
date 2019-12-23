26f0fc15bec3b7656860f23fac289aa2424711eb
Md Rayhan <mdrayhan9464@gmail.com>
2019-12-23 17:36:09 +0600

    tap target view sequence done

--- app/src/main/java/org/richit/taptargetview/MainActivity.java
+++ app/src/main/java/org/richit/taptargetview/MainActivity.java
@@ -1,47 +1,62 @@
 package org.richit.taptargetview;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.media.session.PlaybackStateCompat;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.getkeepsafe.taptargetview.TapTarget;
-import com.getkeepsafe.taptargetview.TapTargetView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-import java.security.PublicKey;
+
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetSequence;
+import com.getkeepsafe.taptargetview.TapTargetView;
 
 import dm.audiostreamer.AudioStreamingManager;
 import dm.audiostreamer.CurrentSessionCallback;
 import dm.audiostreamer.MediaMetaData;
 
-public class MainActivity extends AppCompatActivity implements CurrentSessionCallback, View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements CurrentSessionCallback, View.OnClickListener {
 
     public EditText mEditText;
-    public Button mEnterButton ;
+    public Button mEnterButton;
     public Button mPlayButton;
-    public Button mPauseButton ;
+    public Button mPauseButton;
     public Button mStopButton;
     public AudioStreamingManager streamingManager;
     public MediaMetaData mMediaMetaData;
-    public boolean firstStart ;
+    public boolean firstStart;
+    public Toolbar toolbar;
+    private String TAG = this.getClass().getSimpleName();
+    private int tapTargetCurrentStep;
+    public SharedPreferences sharedPreferences;
 
-    public void initObject(){
+    public void initObject() {
 
         mEditText = findViewById( R.id.editTextxmlid );
         mEnterButton = findViewById( R.id.enterbuttonxml );
         mPlayButton = findViewById( R.id.playbuttonxml );
         mPauseButton = findViewById( R.id.pausebuttonxml );
         mStopButton = findViewById( R.id.stopbuttonxml );
+
+        toolbar = findViewById( R.id.toolbarxml );
+        setSupportActionBar( toolbar );
+        getSupportActionBar().setHomeButtonEnabled( true );
+        getSupportActionBar().setDisplayHomeAsUpEnabled( true );
+        getSupportActionBar().setDisplayShowHomeEnabled( true );
+        getSupportActionBar().setHomeAsUpIndicator( R.drawable.ic_magnify_white_24dp );
+        getSupportActionBar().setDisplayShowTitleEnabled( false );
+
         streamingManager = new AudioStreamingManager();
         mMediaMetaData = new MediaMetaData();
     }
 
-    public void initForSetOnClick(){
+    public void initForSetOnClick() {
 
         mEnterButton.setOnClickListener( this );
         mPlayButton.setOnClickListener( this );
@@ -54,24 +69,19 @@ public class MainActivity extends AppCompatActivity implements CurrentSessionCal
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
 
-        SharedPreferences sharePreference = getSharedPreferences( "PREFS", 0 );
-        firstStart = sharePreference.getBoolean( "first_time_start", true );
-
-        if(firstStart){
-
-            SharedPreferences.Editor editor = sharePreference.edit();
-            editor.putBoolean( "first_time_start", false ).apply();
-            TapTargetView.showFor(this,
-                    TapTarget.forView(findViewById(R.id.dotmainxml),
-                            "This is a target", "We have the best targets, believe me")
-                            .tintTarget( false )
-                            .targetCircleColor( R.color.md_blue_grey_500_25 ));
-        }
-
-
         initObject();
         initForSetOnClick();
         streamingManager = AudioStreamingManager.getInstance( this );
+        sharedPreferences = getSharedPreferences( "PREFS", 0 );
+        firstStart = sharedPreferences.getBoolean( "first_time_start", true );
+
+        if (firstStart) {
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean( "first_time_start", false ).apply();
+
+        }
+        tapTargetViews();
 
     }
 
@@ -79,7 +89,7 @@ public class MainActivity extends AppCompatActivity implements CurrentSessionCal
     public void onStart() {
         super.onStart();
         if (streamingManager != null) {
-            streamingManager.subscribesCallBack(this);
+            streamingManager.subscribesCallBack( this );
         }
     }
 
@@ -130,7 +140,7 @@ public class MainActivity extends AppCompatActivity implements CurrentSessionCal
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
 
             case R.id.enterbuttonxml:
                 mMediaMetaData.setMediaUrl( mEditText.getText().toString() );
@@ -150,4 +160,49 @@ public class MainActivity extends AppCompatActivity implements CurrentSessionCal
         }
     }
 
+    public void tapTargetViews() {
+
+        toolbar.inflateMenu( R.menu.main_menu );
+
+        new TapTargetSequence( this )
+                .targets(
+                        TapTarget
+                                .forToolbarNavigationIcon( toolbar, "Search here" )
+                                .tintTarget( true ),
+
+                        TapTarget
+                                .forToolbarMenuItem( toolbar, R.id.dotmenuxml, "haskfds" ),
+
+                        TapTarget
+                                .forView( mPlayButton, "Click here to play" )
+                                .tintTarget( true ),
+
+                        TapTarget.forView
+                                ( mStopButton, "Click here to stop" )
+
+                ).continueOnCancel( true ).considerOuterCircleCanceled( true ).listener( new TapTargetSequence.Listener() {
+            @Override
+            public void onSequenceFinish() {
+                Log.d( TAG, "onSequenceFinish: " );
+            }
+
+            @Override
+            public void onSequenceStep(TapTarget lastTarget, boolean targetClicked) {
+                tapTargetCurrentStep++;
+                Log.d( TAG, "onSequenceStep: " + tapTargetCurrentStep );
+            }
+
+            @Override
+            public void onSequenceCanceled(TapTarget lastTarget) {
+                Log.d( TAG, "onSequenceCanceled: " + lastTarget );
+            }
+        } ).start();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate( R.menu.main_menu, menu );
+        return super.onCreateOptionsMenu( menu );
+    }
 }
